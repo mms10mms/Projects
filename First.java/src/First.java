2006953ee38f26178ab84b748f840f7e60fb7d9a
Michael <mikemms497@gmail.com>
2026-08-24 22:12:27 -0400

    First commit

---
+++ First.java/src/First.java
@@ -0,0 +1,13 @@
+//Michael Slaughter
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!");
+        System.out.println();
+
+        //My Job / Career Choice
+        System.out.println("Preferred Job Title: AI Security Engineer / Architect");
+        System.out.println("Rationale: A developing field that's getting ahead of itself and needs the proper security practices and structure in place for the ever-growing landscape of cybersecurity.");
+        System.out.println("Median Income: $150,000");
+    }
+}
